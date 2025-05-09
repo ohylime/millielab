fe9a7d3f628d05dde0c39114593e4c6d627c1da3
Emily H <emilyyho33@gmail.com>
2025-05-09 00:47:32 -0400

    Create base layout of page
    Setup main layout of the page with css grids.
    Header and Footer, and middle section has left side as navigation and right side to display what is selected.

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,61 @@ import { useState, JSX } from 'react'
 import './App.css'
 
 import { Button } from './components/buttons/Button'
+import { Home } from './Pages/Home'
 
-const Components: Record<string, JSX.Element> = {
-  BUTTON: <Button />,
-  HOME: <h1>Home</h1>,
-}
+
+const views: { key: string, label: string, component: JSX.Element }[] = [
+  { key: 'HOME', label: 'Home', component: <Home /> },
+]
+
+
+const components: { key: string, label: string, component: JSX.Element }[] = [
+  { key: 'BUTTON', label: 'Button', component: <Button /> },
+]
+
+
+
+const allDisplays = [...views, ...components].reduce((acc, { key, component }) => {
+  acc[key] = component
+  return acc
+}, {} as Record<string, JSX.Element>)
 
 function App() {
-  let [display, setDisplay] = useState<'BUTTON' | 'HOME'>('BUTTON')
+  let [display, setDisplay] = useState<string>('BUTTON')
 
   return (
     <>
-      <div> Header </div>
-      <div className='sideBar'>
+      <header className="header"> Header </header>
+      <nav className='side-bar'>
 
+        <ul>
+          <h4>Views</h4>
+          {views.map(({ key, label }) => (
+            <li key={key}>
+              <div onClick={() => setDisplay(key)}>{label}</div>
+            </li>
+          ))}
 
-      </div>
+          <h4>Components</h4>
+
+          {components.map(({ key, label }) => (
+            <li key={key}>
+              <div onClick={() => setDisplay(key)}>{label}</div>
+            </li>
+          ))}
+
+        </ul>
+
+      </nav >
       <div className='display'>
 
-        <div>
-          Welcome to MillieLab
-        </div>
 
-        <Button onClick={() => setDisplay('BUTTON')}>Button</Button>
-        <Button onClick={() => setDisplay('HOME')}>Home</Button>
         <div className='card'>
 
-          {Components[display]}
+          {allDisplays[display]}
         </div>
       </div>
-      <div className='footer'>Footer </div>
+      <footer className='footer'>Footer </footer>
     </>
   )
 }
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,11 @@
+
+export const Home = () => {
+
+  return (
+    <div>
+      <h1>Welcome to MillieLab</h1>
+      <p>MillieLab is a my personal project where I practice React and build random things</p>
+      <p>Explore the components and random things</p>
+    </div>
+  )
+}
